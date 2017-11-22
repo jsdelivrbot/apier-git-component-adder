aa09b03ef9ce4437306590d596c794e421f45ab9
phytertek <ryan.phytertek@gmail.com>
2017-11-22 02:31:48 -0500

    change to the branch

--- index.js
+++ index.js
@@ -14,11 +14,16 @@ if (!newDirExists) fs.mkdirSync(newDir);
 
 newDirExists = fs.existsSync(newDir);
 console.log(newDirExists);
-
-const commands = [`cd ${newDir}`, 'touch aNewFile.js'];
-const callCommands = spawn(commands.join(' && '));
-callCommands.on('exit', () => {
-  const newFileExists = fs.existsSync(`${newDir}/aNewFile.js`);
-  console.log('New file exist!!!', newFileExists);
-});
+const newFileLoc = `${newDir}/aNewFile.js`;
+let newFileExists = fs.existsSync(newFileLoc);
+if (!newFileExists) {
+  const callCommands = spawn(`cd ${newDir} && touch aNewFile.js`);
+  callCommands.on('exit', () => {
+    newFileExists = fs.existsSync(newFileLoc);
+    console.log('New file exist!!!', newFileExists);
+  });
+} else {
+  console.log('New file already exits!');
+  return;
+}
 // https://github.com/phytertek/apier-todo-component.git
